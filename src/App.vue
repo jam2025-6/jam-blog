fda7a9bad4a3ebd456e3a3b1600299bea69a6cd0
jam <1539924780@qq.com>
2025-08-12 16:15:25 +0800

    build(dependencies): 移除未使用的图标库依赖
    - 删除了 @element-plus/icons-vue、@vicons/antd、@vicons/ionicons5 和 @vicons/tabler 等图标库依赖
    - 优化了项目依赖结构，减少了不必要的依赖项

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,7 @@
   <router-view></router-view>
 </template>
 
-<script setup lang="ts">
-import { useThemeStore } from "./stores/theme";
-const themeStore = useThemeStore();
-
-function getThemeMode() {
-  const currentHour = new Date().getHours();
-
-  // 夜间模式时间：晚上6点到早上6点
-  if (currentHour >= 18 || currentHour < 6) {
-    themeStore.setTheme("dark");
-  } else {
-    themeStore.setTheme("light");
-  }
-}
-getThemeMode();
-</script>
+<script setup lang="ts"></script>
 
 <style>
 #app {
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import "./style.css";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import router from "./router";
+import router from "@/router";
 import "@/assets/style/index.scss"; // 全局样式
-import pinia from "./stores"; // ✅ 引入刚才的 pinia 实例
+import pinia from "@/stores"; // ✅ 引入刚才的 pinia 实例
 
-import SvgIcon from "@/components/SvgIcon/index.vue";
+// import SvgIcon from "@/components/SvgIcon/index.vue";
 
 import "virtual:svg-icons-register"; // svg图标
 import dayjs from "dayjs";
